d45acfc5f90934bd4787c9bd659f42ed8064371c
William Kwan <william@voidupdate.com>
2015-11-26 11:31:03 -0500

    scale buttons onclick

---
+++ Assets/_Scripts/ScaleButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ScaleButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler 
+{
+	RectTransform buttonRect;
+	float scaleDuration = 0.15f;
+	
+	void Start()
+	{
+		buttonRect = GetComponent<RectTransform>();
+	}
+	
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		buttonRect.DOScale(new Vector3(1.2f, 1.2f, 1.2f), scaleDuration);
+	}
+	
+	
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		buttonRect.DOScale(new Vector3(1f, 1f, 1f), scaleDuration);
+		
+	}
+
+}
+
