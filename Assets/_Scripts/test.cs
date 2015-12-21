30a162226c55f1f8a696c6d1d23f0f6d50abd54c
William Kwan <william@voidupdate.com>
2015-12-21 18:39:46 -0500

    glowing cube working again

---
+++ Assets/_Scripts/test.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+	
+	public Color albedoBlue;
+	public Color emissionBlue;
+
+	// Use this for initialization
+	void Start () {
+		MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+		meshRenderer.material.color = new Color(0.3f, 0.5f, 0.5f);
+		meshRenderer.material.EnableKeyword("_EMISSION");
+		meshRenderer.material.SetColor("_EmissionColor", emissionBlue);
+		Debug.Log("hey");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
